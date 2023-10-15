483799e3f1ebfc7e2dd6a9388577a41118dd894c
DylanHalstead <dylanhalstead11@gmail.com>
2023-10-14 22:11:12 -0400

    added comments for future items

--- app.py
+++ app.py
@@ -24,10 +24,13 @@ def generate_board():
   detail_level = request.form.get('detail_level')
   point_system = request.form.get('point_system') or 'Fibonacci Sequence (0, 1, 2, 3, 5)'
 
-  test = [1, 2, 3, 4, 5]
+  # {{#select "sprint_num" logprobs='logprobs'}}3{{or}}4{{or}}5{{/select}}
+  # {{#if suggested_time}} I planned on spending {{suggested_time}} on this project. {{/if}} 
+  # {{#if sprint_suggestions}}Make sure to include: {{#each sprint_suggestions}}{{this.suggestion}} in {{this.sprint}},{{/each}}{{/if}} 
+  # {{#if suggestions.timeframe}}Break up into 4, 2-week sprint intervals{{/if}} 
   sprint_name_query = guidance('''
     I'm making a project titled "{{title}}" with the description "{{description}}". How many sprints should I have to get started?
-    {{#select "sprint_num" logprobs='logprobs'}}3{{or}}4{{or}}5{{/select}}
+    {{#select "sprint_num"}}3{{or}}4{{or}}5{{/select}}
 
     Give me names for each sprint (make a maximum of {{sprint_num}} sprints):
     {{#geneach 'sprint_titles' }}
@@ -39,6 +42,10 @@ def generate_board():
   sprint_titles = results['sprint_titles']
   sprint_titles = [title.split('\n')[0].strip() for title in sprint_titles]
 
+  # {{#if suggested_tools}}I planned on using "{{suggested_tools}}" in the project. {{/if}}
+  # {{#if suggestions.sprint_suggestions}}Make sure to include: {{#each suggestions.sprint_suggestions}}{{this.suggestion}} in {{this.sprint}},{{/each}}{{/if}} 
+  # {{#if suggestions.timeframe}}Break up into 4, 2-week sprint intervals{{/if}} 
+  # {{#if suggestions.team}}This project is for a team of {{suggestions.team.size}} who can finish roughly {{suggestions.team.velocity}} per sprint.{{/if}}
   card_query = guidance('''
     I'm making a project titled "{{title}}" with the description "{{description}}". Create me a {{detail_level}} storyboard with cards describing what needs to be done to complete the project.
     Use {{point_system}} for story points:

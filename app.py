eeb89733ad5f2deafdbd9f52122179ae656c6082
DylanHalstead <dylanhalstead11@gmail.com>
2023-12-14 21:03:09 -0500

    idk man

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ client = OpenAI(
   timeout=Timeout(120.0, read=120.0, write=60.0, connect=60.0)
 )
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, support_credentials=True)
 
 @app.get("/")
-@cross_origin()
+@cross_origin(support_credentials=True)
 def hello_world():
   return {"hello": "world"}
 
 @app.post("/api/generate/board")
-@cross_origin()
+@cross_origin(support_credentials=True)
 def generate_board():
   title=request.form.get('title')
   description=request.form.get('description')
@@ -66,7 +66,7 @@ def generate_board():
     return jsonify({"message": result})
 
 @app.post("/api/generate/card")
-@cross_origin()
+@cross_origin(support_credentials=True)
 def generate_card():
   if not request.is_json:
     return jsonify({"message": "Request body must be JSON"}), 400
